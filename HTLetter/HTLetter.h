61602d25ed2f6d1e4a31137e0423eabea265266d
fallen.ink <fengzilijie@gmail.com>
2015-11-16 14:20:52 +0800

    +

---
+++ HTLetter/HTLetter.h
@@ -0,0 +1,18 @@
+//
+//  HTLetter.h
+//  QQing
+//
+//  Created by 李杰 on 9/17/15.
+//
+//
+
+#ifndef QQing_HTLetter_h
+#define QQing_HTLetter_h
+
+#import "NSArray+HTLetter.h"
+#import "NSString+HTLetter.h"
+
+#import "Pinyin.h"
+#import "POAPinyin.h"
+
+#endif
