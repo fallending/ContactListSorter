5dd1fba57966217ab194d88877f791c864f3f26b
fallen.ink <fengzilijie@gmail.com>
2015-11-16 16:22:16 +0800

    +

--- HTLetter/pinyin.h
+++ HTLetter/pinyin.h
@@ -1,17 +1,47 @@
-/*
- *  pinyin.h
- *  Chinese Pinyin First Letter
- *
- *  Created by George on 4/21/10.
- *  Copyright 2010 RED/SAFI. All rights reserved.
- *
- */
+//
+//  POAPinyin.h
+//  POA
+//
+//  Created by haung he on 11-7-18.
+//  Copyright 2011年 huanghe. All rights reserved.
+//
+
 #import <Foundation/Foundation.h>
 
-char pinyinFirstLetter(unsigned short hanzi);
+@class PinyinMapObject;
 
-@interface HTLetters : NSObject
+@interface Pinyin : NSObject
 
+/**
+ *  @input
+ *  中文汉字
+ 
+ *  @return
+ *  全拼
+ */
++ (NSString *)pinyinWithLetterChn:(NSString *)chnString;
+
+/**
+ *  返回tableview右方indexArray
+ */
++ (NSMutableArray *)indexWithStringArray:(NSArray *)strings;
+
+//-----  返回联系人
+/**
+ *
+ */
++ (NSMutableArray *)LetterSortArray:(NSArray *)stringArr;
+
+
+///----------------------
+//返回一组字母排序数组(中英混排)
++ (NSMutableArray *)SortArray:(NSArray *)stringArr;
+
+@end
+
+#pragma mark - 拼音首字母
+
+@interface Pinyin ( FirstLetter )
 /**
  *  @desc
  *  获取汉字首字母，如果参数既不是汉字也不是英文字母，则返回 @“#”
@@ -19,7 +49,7 @@ char pinyinFirstLetter(unsigned short hanzi);
  *  @goal
  *  主要用于生成，通讯录右侧的indexes
  */
-+ (NSString *)firstLetter:(NSString *)chineseString;
++ (NSString *)firstLetter:(NSString *)chnString;
 
 /**
  *  @desc
@@ -28,6 +58,18 @@ char pinyinFirstLetter(unsigned short hanzi);
  *  @goal
  *  主要用于生成，通讯录右侧的indexes
  */
-+ (NSString *)firstLetters:(NSString *)chineseString;
++ (NSString *)firstLetters:(NSString *)chnString;
+
+
+@end
+
+#pragma mark -
+
+@interface PinyinMapObject : NSObject
+
++ (NSArray<PinyinMapObject *> *)map;
+
+@property (nonatomic, strong) NSString *pinyin; // 拼音
+@property (nonatomic, strong) NSString *pinyinChn; // 汉子
 
 @end
