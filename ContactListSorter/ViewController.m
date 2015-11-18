dd16295a0dbd719ca2a56f167234be604615f7af
fallen.ink <fengzilijie@gmail.com>
2015-11-18 16:57:23 +0800

    +

--- ContactListSorter/ViewController.m
+++ ContactListSorter/ViewController.m
@@ -12,12 +12,19 @@
 @interface ViewController () <
     UITableViewDelegate,
     UITableViewDataSource,
-    GDIIndexBarDelegate
+    GDIIndexBarDelegate,
+    UISearchBarDelegate,
+    UISearchControllerDelegate,
+    UISearchResultsUpdating
 >
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
+// 名字索引
 @property (nonatomic, strong) GDIIndexBar *                 indexBar;
+
+// 搜索栏
+@property (nonatomic, strong) UISearchController *searchController;
 
 @end
 
@@ -122,6 +129,120 @@
     [self.tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:index]
                           atScrollPosition:UITableViewScrollPositionTop
                                   animated:YES];
+}
+
+#pragma mark - UISearchBarDelegate
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)aSearchBar {
+    [aSearchBar resignFirstResponder];
+}
+
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)aSearchBar {
+    [aSearchBar setShowsCancelButton:YES animated:YES];
+}
+
+- (void)searchBarTextDidEndEditing:(UISearchBar *)aSearchBar {
+    [aSearchBar setShowsCancelButton:NO animated:YES];
+}
+
+#pragma mark - UISearchControllerDelegate
+
+
+
+#pragma mark - UISearchResultsUpdating
+
+- (void)updateSearchResultsForSearchController:
+(UISearchController *)searchController {
+//    // update the filtered array based on the search text
+//    NSString *searchText = searchController.searchBar.text;
+//    NSMutableArray *searchResults = [[self.partitionedContacts
+//                                      valueForKeyPath:@"@unionOfArrays.self"] mutableCopy];
+//    
+//    // strip out all the leading and trailing spaces
+//    NSString *strippedStr =
+//    [searchText stringByTrimmingCharactersInSet:
+//     [NSCharacterSet whitespaceCharacterSet]];
+//    
+//    // break up the search terms (separated by spaces)
+//    NSArray *searchItems = nil;
+//    if (strippedStr.length > 0) {
+//        searchItems = [strippedStr componentsSeparatedByString:@" "];
+//    }
+//    // build all the "AND" expressions for each value in the searchString
+//    NSMutableArray *andMatchPredicates = [NSMutableArray array];
+//    
+//    for (NSString *searchString in searchItems) {
+//        NSMutableArray *searchItemsPredicate = [NSMutableArray array];
+//        
+//        // TODO: match phone number matching
+//        
+//        // name field matching
+//        NSPredicate *finalPredicate = [NSPredicate
+//                                       predicateWithFormat:@"compositeName CONTAINS[c] %@", searchString];
+//        [searchItemsPredicate addObject:finalPredicate];
+//        
+//        NSPredicate *predicate =
+//        [NSPredicate predicateWithFormat:@"ANY SELF.emails CONTAINS[c] %@",
+//         searchString];
+//        [searchItemsPredicate addObject:predicate];
+//        
+//        predicate = [NSPredicate
+//                     predicateWithFormat:@"ANY SELF.addresses.street CONTAINS[c] %@",
+//                     searchString];
+//        [searchItemsPredicate addObject:predicate];
+//        predicate = [NSPredicate
+//                     predicateWithFormat:@"ANY SELF.addresses.city CONTAINS[c] %@",
+//                     searchString];
+//        [searchItemsPredicate addObject:predicate];
+//        predicate = [NSPredicate
+//                     predicateWithFormat:@"ANY SELF.addresses.zip CONTAINS[c] %@",
+//                     searchString];
+//        [searchItemsPredicate addObject:predicate];
+//        predicate = [NSPredicate
+//                     predicateWithFormat:@"ANY SELF.addresses.country CONTAINS[c] %@",
+//                     searchString];
+//        [searchItemsPredicate addObject:predicate];
+//        predicate = [NSPredicate
+//                     predicateWithFormat:
+//                     @"ANY SELF.addresses.countryCode CONTAINS[c] %@", searchString];
+//        [searchItemsPredicate addObject:predicate];
+//        
+//        //        NSNumberFormatter *numFormatter = [[NSNumberFormatter alloc]
+//        //        init];
+//        //        [numFormatter setNumberStyle:NSNumberFormatterNoStyle];
+//        //        NSNumber *targetNumber = [numFormatter
+//        //        numberFromString:searchString];
+//        //        if (targetNumber != nil) {   // searchString may not convert
+//        //        to a number
+//        //            predicate = [NSPredicate predicateWithFormat:@"ANY
+//        //            SELF.sanitizePhones CONTAINS[c] %@", searchString];
+//        //            [searchItemsPredicate addObject:predicate];
+//        //        }
+//        
+//        // at this OR predicate to our master AND predicate
+//        NSCompoundPredicate *orMatchPredicates =
+//        (NSCompoundPredicate *)[NSCompoundPredicate
+//                                orPredicateWithSubpredicates:searchItemsPredicate];
+//        [andMatchPredicates addObject:orMatchPredicates];
+//    }
+//    
+//    NSCompoundPredicate *finalCompoundPredicate = nil;
+//    
+//    // match up the fields of the Product object
+//    finalCompoundPredicate = (NSCompoundPredicate *)
+//    [NSCompoundPredicate andPredicateWithSubpredicates:andMatchPredicates];
+//    
+//    searchResults = [[searchResults
+//                      filteredArrayUsingPredicate:finalCompoundPredicate] mutableCopy];
+//    
+//    // hand over the filtered results to our search results table
+//    ZLResultsTableViewController *tableController =
+//    (ZLResultsTableViewController *)
+//    self.searchController.searchResultsController;
+//    tableController.filedMask = self.filedMask;
+//    tableController.selectedPeople = self.selectedPeople;
+//    [tableController setPartitionedContactsWithContacts:searchResults];
+//    [tableController.tableView reloadData];
 }
 
 #pragma mark - Property
